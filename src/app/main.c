cf60e7ef1f1208202667252be405a3e6cf51a3fd
shuo <3292226889@qq.com>
2026-06-02 22:00:11 +0800

    小改

--- src/app/main.c
+++ src/app/main.c
@@ -4,22 +4,18 @@
 
 int tt = 0;
 
-void vTaskLED(void *pvParameters) {
+static void task_led(void *pvParameters) {
     while(1) {
-        // 翻转 LED (请根据你的 SysConfig 命名修改)
         DL_GPIO_togglePins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_PIN);
         tt++;
-        vTaskDelay(pdMS_TO_TICKS(500)); // 延时 500ms
+        vTaskDelay(pdMS_TO_TICKS(1000)); // 延时 1000ms
     }
 }
 
 int main(void) {
-    SYSCFG_DL_init(); // 初始化 TI 硬件
+    SYSCFG_DL_init();
     
-    // 清除 SysTick 配置，FreeRTOS 会自己接管
-    // 如果你在 SysConfig 里配置了定时器，请确保不要与 FreeRTOS 冲突
-    
-    xTaskCreate(vTaskLED, "LED_Task", 128, NULL, 1, NULL);
+    xTaskCreate(task_led, "LED_Task", 128, NULL, 1, NULL);
     
     vTaskStartScheduler();
     
