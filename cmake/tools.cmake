92ef26c150f1dd11d5526e78b297626546acb867
shuo <3292226889@qq.com>
2026-06-04 21:00:54 +0800

    编译优化

--- cmake/tools.cmake
+++ cmake/tools.cmake
@@ -7,9 +7,15 @@ set(TAG "Tools")
 set(SYSCFG_INSTALL_DIR "${CMAKE_SOURCE_DIR}/tools/sysconfig" CACHE PATH "Directory for installing syscfg tool")
 set(SYSCFG_GEN_DIR "${CMAKE_CURRENT_SOURCE_DIR}/src/syscfg" CACHE PATH "Directory for generated syscfg files")
 
+set(SYSCFG_FILE "${CMAKE_CURRENT_SOURCE_DIR}/config/${PROJECT_NAME}.syscfg")
 set(SYSCFG_CLI "${SYSCFG_INSTALL_DIR}/sysconfig_cli.sh")
 set(SYSCFG_GUI "${SYSCFG_INSTALL_DIR}/sysconfig_gui.sh")
 set(PRODUCT_JSON_FILE "${CMAKE_SOURCE_DIR}/tools/product.json")
+set(SYSCFG_GEN_FILES
+    "${SYSCFG_GEN_DIR}/ti_msp_dl_config.c"
+    "${SYSCFG_GEN_DIR}/ti_msp_dl_config.h"
+    "${SYSCFG_GEN_DIR}/device.opt"
+)
 
 print_blankline()
 print_item(${TAG} "SysConfig install directory: ${SYSCFG_INSTALL_DIR}")
@@ -17,8 +23,6 @@ print_item(${TAG} "SysConfig CLI: ${SYSCFG_CLI}")
 print_item(${TAG} "SysConfig GUI: ${SYSCFG_GUI}")
 
 function(syscfg_gen)
-    set(SYSCFG_FILE "${CMAKE_CURRENT_SOURCE_DIR}/config/${PROJECT_NAME}.syscfg")
-
     set(TEMP_GEN_DIR "${CMAKE_BINARY_DIR}/syscfg_temp")
     file(MAKE_DIRECTORY "${TEMP_GEN_DIR}")
 
@@ -33,19 +37,24 @@ function(syscfg_gen)
         --product "${PRODUCT_JSON_FILE}"
         --output "${TEMP_GEN_DIR}"
         --quiet
-        "${SYSCFG_FILE}"
+        "${SYSCFG_FILE}" > /dev/null 2>&1
 
         COMMAND ${CMAKE_COMMAND} -E copy "${TEMP_GEN_DIR}/ti_msp_dl_config.c" "${SYSCFG_GEN_DIR}/"
         COMMAND ${CMAKE_COMMAND} -E copy "${TEMP_GEN_DIR}/ti_msp_dl_config.h" "${SYSCFG_GEN_DIR}/"
         COMMAND ${CMAKE_COMMAND} -E copy "${TEMP_GEN_DIR}/device.opt" "${SYSCFG_GEN_DIR}/"
 
         COMMAND ${CMAKE_COMMAND} -E echo ""
-        COMMAND ${CMAKE_COMMAND} -E echo "${ColorGreen}[SysConfig]${ColorReset} Configuration file setup for ${SYSCFG_GEN_DIR}"
+        COMMAND ${CMAKE_COMMAND} -E echo "-- ${ColorGreen}[SysConfig]${ColorReset} Configuration file setup for ${SYSCFG_GEN_DIR}"
+        COMMAND ${CMAKE_COMMAND} -E echo ""
 
         DEPENDS "${SYSCFG_FILE}"
 
-        COMMENT "Generating and cleaning SysConfig files..." 
+        COMMENT "Generating and cleaning SysConfig files..."
 
         VERBATIM
+    )
+
+    add_custom_target(syscfg_gen_target
+        DEPENDS ${SYSCFG_GEN_FILES}
     )
 endfunction()
