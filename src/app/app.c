2889bbe8e8c59fc82b912f52c93614f956765ab9
shuo <3292226889@qq.com>
2026-06-04 18:08:13 +0800

    buzzer 机制改动，可循环播放

--- src/app/app.c
+++ src/app/app.c
@@ -20,7 +20,8 @@ Joystick* joystick = NULL;
 
 void App_Init(void) {
     // Led_simple_config led_cfg = {
-    //     .gpio_idx = 0, .use_as_indicator = 1, .blink_freq_hz = 2, .gpio_state_when_on = bsp_gpio_state_set};
+    //     .gpio_idx = 0, .use_as_indicator = 1, .blink_freq_hz = 2, .gpio_state_when_on =
+    //     bsp_gpio_state_set};
     // led_indicator = Led_Simple_Create(&led_cfg);
 
     // Led_breath_config led_breath_cfg = {.pwm_idx = 0, .max_brightness = 100, .breath_freq_hz = 1.0f};
@@ -30,10 +31,11 @@ void App_Init(void) {
     button1 = Button_Create(&btn_cfg);
     btn_cfg.gpio_idx = 0;
     btn_cfg.gpio_state_when_pressed = bsp_gpio_state_reset;
-    button2=Button_Create(&btn_cfg);
+    button2 = Button_Create(&btn_cfg);
 
     Buzzer_config buzzer_cfg = {
-        .pwm_idx = 0, .music_score = music1, .score_length = music1_len, .speed_npm = 60 * 10};
+        .pwm_idx = 0,
+    };
     buzzer = Buzzer_Create(&buzzer_cfg);
 
     Joystick_config joystick_cfg = {
@@ -51,6 +53,6 @@ void App_Loop(void) {
 
     if (Button_Get_State(button2) != last_button_state) {
         last_button_state = Button_Get_State(button2);
-        if (last_button_state == button_state_down) { Buzzer_Play(buzzer); }
+        if (last_button_state == button_state_down) { Buzzer_Play(buzzer, music1, music1_len, 60 * 10, 0); }
     }
 }
