7764285bbd9eec6085396b56527e400c0e6b3c3a
zzs <3292226889@qq.com>
2026-06-06 04:05:07 +0800

    跨平台适配

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,14 +1,20 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
+if(CMAKE_HOST_WIN32)
+    set(EXEC_SUFFIX ".exe" CACHE STRING "Executable suffix for Windows")
+else()
+    set(EXEC_SUFFIX "" CACHE STRING "Executable suffix for Unix-like systems")
+endif()
+
 set(TOOLCHAIN_BIN_DIR "${CMAKE_CURRENT_SOURCE_DIR}/tools/gcc-arm-none-eabi/bin")
 set(LINKER_LIBS_PATH "${CMAKE_CURRENT_SOURCE_DIR}/tools/gcc-arm-none-eabi/arm-none-eabi/lib/thumb/v6-m/nofp")
 
-set(CMAKE_C_COMPILER   "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-gcc")
-set(CMAKE_ASM_COMPILER "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-gcc")
-set(CMAKE_CXX_COMPILER "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-g++")
-set(CMAKE_OBJCOPY      "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-objcopy")
-set(CMAKE_SIZE         "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-size")
+set(CMAKE_C_COMPILER   "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-gcc${EXEC_SUFFIX}")
+set(CMAKE_ASM_COMPILER "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-gcc${EXEC_SUFFIX}")
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-g++${EXEC_SUFFIX}")
+set(CMAKE_OBJCOPY      "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-objcopy${EXEC_SUFFIX}")
+set(CMAKE_SIZE         "${TOOLCHAIN_BIN_DIR}/arm-none-eabi-size${EXEC_SUFFIX}")
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
