f7601e49b5ecb5a1005f5435299ebd73da039396
shuo <3292226889@qq.com>
2026-06-07 02:14:17 +0800

    dma 中断

--- src/it.c
+++ src/it.c
@@ -1,5 +1,8 @@
 #include "bsp_adc.h"
+#include "bsp_spi.h"
 #include "devices/msp/m0p/mspm0g350x.h"
 #include "ti_msp_dl_config.h"
 
 void ADC12_0_INST_IRQHandler(void) { Bsp_Adc_Irq_Handler(ADC0); }
+
+void SPI1_IRQHandler(void) { Bsp_Spi_Irq_Handler(SPI1); }
