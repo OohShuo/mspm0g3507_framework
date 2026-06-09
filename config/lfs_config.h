dac1ba382380fba9d770c667318020a3587739e9
shuo <3292226889@qq.com>
2026-06-09 17:48:18 +0800

    lfs_test 测试通过

--- config/lfs_config.h
+++ config/lfs_config.h
@@ -2,10 +2,12 @@
 
 #include "SEGGER_RTT.h"
 
+#define LFS_NO_MALLOC
+
 #define LFS_TRACE(fmt, ...) SEGGER_RTT_printf(0, "%s:%d:trace: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define LFS_DEBUG(fmt, ...) SEGGER_RTT_printf(0, "%s:%d:debug: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
-#define LFS_WARN(fmt, ...)  SEGGER_RTT_printf(0, "%s:%d:warn: "  fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define LFS_WARN(fmt, ...)  SEGGER_RTT_printf(0, "%s:%d:warn: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define LFS_ERROR(fmt, ...) SEGGER_RTT_printf(0, "%s:%d:error: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
