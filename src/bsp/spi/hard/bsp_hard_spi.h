c44025bf3fab5936ca32def1211453f789364df3
shuo <3292226889@qq.com>
2026-06-10 00:30:12 +0800

    bsp_uart added 待测

---
+++ src/bsp/spi/hard/bsp_hard_spi.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "devices/msp/peripherals/hw_spi.h"
+
+typedef void (*Bsp_spi_tx_dma_done_cb_t)(void* arg);
+typedef void (*Bsp_spi_tx_done_cb_t)(void* arg);
+typedef void (*Bsp_spi_rx_dma_done_cb_t)(void* arg);
+typedef void (*Bsp_spi_idle_cb_t)(void* arg);
+
+void Bsp_Hard_Spi_Init(void);
+
+void Bsp_Hard_Spi_Wait_For_Complete(uint32_t idx);
+
+// read / write
+void Bsp_Hard_Spi_Write(uint32_t idx, const uint8_t* data, uint32_t len);
+void Bsp_Hard_Spi_Read(uint32_t idx, uint8_t* data, uint32_t len);
+void Bsp_Hard_Spi_Write_Blocking(uint32_t idx, const uint8_t* data, uint32_t len);
+void Bsp_Hard_Spi_Read_Blocking(uint32_t idx, uint8_t* data, uint32_t len);
+
+// callback registration
+void Bsp_Hard_Spi_Register_Tx_Dma_Done_Cb(uint32_t idx, Bsp_spi_tx_dma_done_cb_t cb, void* cb_arg);
+void Bsp_Hard_Spi_Register_Tx_Done_Cb(uint32_t idx, Bsp_spi_tx_done_cb_t cb, void* cb_arg);
+void Bsp_Hard_Spi_Register_Rx_Dma_Done_Cb(uint32_t idx, Bsp_spi_rx_dma_done_cb_t cb, void* cb_arg);
+void Bsp_Hard_Spi_Register_Idle_Cb(uint32_t idx, Bsp_spi_idle_cb_t cb, void* cb_arg);
+
+void Bsp_Hard_Spi_Irq_Handler(SPI_Regs* spi_inst);
--- src/it.c
+++ src/it.c
@@ -1,13 +1,16 @@
 #include "FreeRTOS.h"
 #include "bsp_adc.h"
 #include "bsp_spi.h"
+#include "bsp_uart.h"
 #include "rtt_log.h"
 #include "task.h"
 #include "ti_msp_dl_config.h"
 
 void ADC12_0_INST_IRQHandler(void) { Bsp_Adc_Irq_Handler(ADC0); }
 
-void SPI1_IRQHandler(void) { Bsp_Spi_Irq_Handler(SPI1); }
+void SPI1_IRQHandler(void) { Bsp_Hard_Spi_Irq_Handler(SPI1); }
+
+void UART0_INST_IRQHandler(void) { Bsp_Uart_Irq_Handler(UART0); }
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
     printf("STACK OVERFLOW: %s\n", pcTaskName);
