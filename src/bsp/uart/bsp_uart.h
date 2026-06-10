6126e26b92fabad6004a319047212f36fbded71b
shuo <3292226889@qq.com>
2026-06-10 19:01:27 +0800

    uart 变长数据接收

--- src/bsp/uart/bsp_uart.h
+++ src/bsp/uart/bsp_uart.h
@@ -4,12 +4,12 @@
 
 #include "devices/msp/peripherals/hw_uart.h"
 
-#define BSP_UART_CONTINUOUS_RX_BUF_SIZE 256
+#define BSP_UART_CONTINUOUS_RX_BUF_SIZE 16
 
 typedef void (*Bsp_uart_tx_dma_done_cb_t)(void* arg);
 typedef void (*Bsp_uart_tx_done_cb_t)(void* arg);
 typedef void (*Bsp_uart_rx_dma_done_cb_t)(void* arg);
-typedef void (*Bsp_uart_rx_idle_cb_t)(uint32_t idx, uint8_t* data, uint32_t len);
+typedef void (*Bsp_uart_rx_idle_cb_t)(uint32_t idx, uint8_t* data, uint32_t len, void* arg);
 
 void Bsp_Uart_Init(void);
 
@@ -20,14 +20,14 @@ void Bsp_Uart_Write(uint32_t idx, const uint8_t* data, uint32_t len);
 void Bsp_Uart_Read(uint32_t idx, uint8_t* data, uint32_t len);
 void Bsp_Uart_Write_Blocking(uint32_t idx, const uint8_t* data, uint32_t len);
 void Bsp_Uart_Read_Blocking(uint32_t idx, uint8_t* data, uint32_t len);
-void Bsp_Uart_Start_Continuous_Rx(uint32_t idx, uint32_t idle_timeout_ms);
+void Bsp_Uart_Start_Continuous_Rx(uint32_t idx, uint32_t idle_timeout_ms, uint8_t* buf, uint32_t max_len);
 void Bsp_Uart_Stop_Continuous_Rx(uint32_t idx);
 
 // callback registration
 void Bsp_Uart_Register_Tx_Dma_Done_Cb(uint32_t idx, Bsp_uart_tx_dma_done_cb_t cb, void* cb_arg);
 void Bsp_Uart_Register_Tx_Done_Cb(uint32_t idx, Bsp_uart_tx_done_cb_t cb, void* cb_arg);
 void Bsp_Uart_Register_Rx_Dma_Done_Cb(uint32_t idx, Bsp_uart_rx_dma_done_cb_t cb, void* cb_arg);
-void Bsp_Uart_Register_Rx_Idle_Cb(uint32_t idx, Bsp_uart_rx_idle_cb_t cb);
+void Bsp_Uart_Register_Rx_Idle_Cb(uint32_t idx, Bsp_uart_rx_idle_cb_t cb, void* cb_arg);
 
 void Bsp_Uart_Irq_Handler(UART_Regs* uart_inst);
 void Bsp_Uart_Idle_Irq_Handler(uint32_t idx);
--- src/it.c
+++ src/it.c
@@ -10,9 +10,17 @@ void ADC12_0_INST_IRQHandler(void) { Bsp_Adc_Irq_Handler(ADC0); }
 
 void SPI1_IRQHandler(void) { Bsp_Hard_Spi_Irq_Handler(SPI1); }
 
-void UART0_INST_IRQHandler(void) { Bsp_Uart_Irq_Handler(UART0); }
+void UART0_IRQHandler(void) { Bsp_Uart_Irq_Handler(UART0); }
 
-void TIMA1_IRQHandler(void) { Bsp_Uart_Idle_Irq_Handler(0); }
+void TIMA1_IRQHandler(void) {
+    switch (DL_TimerA_getPendingInterrupt(TIMA1)) {
+        case DL_TIMERA_INTERRUPT_ZERO_EVENT:
+            Bsp_Uart_Idle_Irq_Handler(0);
+            break;
+        default:
+            break;
+    }
+}
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
     printf("STACK OVERFLOW: %s\n", pcTaskName);
