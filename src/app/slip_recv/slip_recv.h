41cfe4282b0848b2328166dd4a954a7bd71ac6e4
shuo <3292226889@qq.com>
2026-06-12 01:07:19 +0800

    长数据 10 Hz 发送，测试通过

---
+++ src/app/slip_recv/slip_recv.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void App_Slip_Recv_Init(void);
+void App_Slip_Recv_Loop(void);
--- src/hal/com_uart/com_uart.h
+++ src/hal/com_uart/com_uart.h
@@ -9,21 +9,21 @@ typedef struct Com_uart_t Com_uart;
 typedef void (*Com_uart_on_rx_t)(Com_uart* obj, const uint8_t* data, uint32_t len, void* arg);
 
 typedef struct {
-    uint32_t uart_idx;          
+    uint32_t uart_idx;
 
-    uint32_t idle_timeout_ms;   
-    uint32_t rx_max_len;        
-    uint32_t tx_max_len;        
-    Com_uart_on_rx_t on_rx;     
+    uint32_t idle_timeout_ms;
+    uint32_t rx_max_len;
+    uint32_t tx_max_len;
+    Com_uart_on_rx_t on_rx;
     void* on_rx_arg;
 } Com_uart_config;
 
 struct Com_uart_t {
     Com_uart_config config;
-    uint8_t* tx_buf;        
-    uint8_t* rx_buf;        
-    General_data rx_data;          
-    uint8_t rx_update_flag;       
+    uint8_t* tx_buf;
+    uint8_t* rx_buf;
+    General_data rx_data;
+    uint8_t rx_update_flag;
 };
 
 void Com_Uart_Init(void);
