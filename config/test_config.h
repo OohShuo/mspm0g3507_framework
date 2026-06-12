907960034220d0b7e15f9de520e9ed6e238d015b
shuo <3292226889@qq.com>
2026-06-13 00:36:38 +0800

    test 组织，过编译

--- config/test_config.h
+++ config/test_config.h
@@ -1,3 +1,11 @@
 #pragma once
 
-#define TEST_COM_UART_ENABLE 1
+#define TEST_COM_UART_ENABLE     0
+#define TEST_LCD_ENABLE          0
+#define TEST_LFS_ENABLE          0
+#define TEST_LVGL_BALL_ENABLE    1
+#define TEST_LVGL_HELLO_ENABLE   1
+#define TEST_RTT_ENABLE          0
+#define TEST_SLIP_RECV_ENABLE    0
+#define TEST_ST7789_IMG_ENABLE   1
+#define TEST_W25Q32_ENABLE       0
--- src/test/st7789_img_test/st7789_img_test.h
+++ src/test/st7789_img_test/st7789_img_test.h
@@ -1,19 +1,3 @@
 #pragma once
 
-#include <stdint.h>
-
-// Wire the LCD instance and run the ST7789 init sequence. Must be called
-// from a task (the init uses busy-wait delays but no FreeRTOS APIs).
-void App_St7789_Img_Test_Init(void);
-
-// Step the image-display test. Call from a periodic task. Idempotent when
-// the previous flush has not finished.
-void App_St7789_Img_Test_Loop(void);
-
-typedef struct {
-    const char* image_name;
-    uint32_t flush_count;
-    uint8_t init_done;
-} St7789_img_test_status;
-
-const St7789_img_test_status* App_St7789_Img_Test_Get_Status(void);
+void St7789_Img_Test_Task_Def(void);
