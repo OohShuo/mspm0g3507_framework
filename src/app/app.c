4075fa6f902c7cbb60250fab7285793e78cdb644
Morro <morrowhome0712@outlook.com>
2026-06-14 12:05:27 +0800

    feat: add lightweight Pac-Man game

--- src/app/app.c
+++ src/app/app.c
@@ -2,11 +2,15 @@
 #include "app_config.h"
 
 #include "flash_mgr.h"
+#include "pacman.h"
 
 void App_Init(void) {}
 
-void App_Task_Def(void) { 
-    #if FLASH_MGR_ENABLE
-    Flash_Mgr_Task_Def(); 
-    #endif
+void App_Task_Def(void) {
+#if FLASH_MGR_ENABLE
+    Flash_Mgr_Task_Def();
+#endif
+#if PACMAN_GAME_ENABLE
+    Pacman_Task_Def();
+#endif
 }
