4c14df4de047856aad50a68ab9e9f0fdc6343722
Morro <morrowhome0712@outlook.com>
2026-06-15 01:44:39 +0800

    使用原始Flash缓存保持飞机大战高清画质

--- src/app/image_asset/image_asset.h
+++ src/app/image_asset/image_asset.h
@@ -16,11 +16,15 @@ typedef struct {
     uint16_t height;
     uint8_t flags;
     uint8_t is_open;
+    uint16_t asset_id;
     uint32_t pixel_data_size;
     uint32_t next_read_offset;
+    uint32_t raw_pixel_address;
 } Image_asset;
 
 uint8_t Image_Asset_Open(Image_asset* image, const char* path);
 void Image_Asset_Close(Image_asset* image);
+uint8_t Image_Asset_Prepare_Raw_Cache(
+    Image_asset* image, uint32_t address, uint32_t capacity);
 uint8_t Image_Asset_Read_Span(Image_asset* image, uint16_t y, uint16_t x,
     uint16_t width, uint16_t* pixels);
--- src/app/storage/storage.h
+++ src/app/storage/storage.h
@@ -9,6 +9,9 @@
 uint8_t Storage_Init(void);
 uint8_t Storage_Is_Available(void);
 uint8_t Storage_Format(void);
+uint8_t Storage_Raw_Read(uint32_t address, void* data, uint32_t size);
+uint8_t Storage_Raw_Write(uint32_t address, const void* data, uint32_t size);
+uint8_t Storage_Raw_Erase(uint32_t address, uint32_t size);
 
 #if FRAMEWORK_USE_LFS
 lfs_t* Storage_Get_Lfs(void);
