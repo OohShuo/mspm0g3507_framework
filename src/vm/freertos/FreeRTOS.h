02503c7579d352d94ddddc03d84a5c81cb041060
shuo <3292226889@qq.com>
2026-06-16 03:43:15 +0800

    完善虚拟机

--- src/vm/freertos/FreeRTOS.h
+++ src/vm/freertos/FreeRTOS.h
@@ -1,8 +1,16 @@
 #pragma once
 #include <stdint.h>
 #include <stddef.h>
-#include <assert.h>
-#define configASSERT(x) assert(x)
+#include <stdio.h>
+#include <stdlib.h>
+
+// Let FreeRTOSConfig.h define configASSERT first (avoids redefinition warning),
+// then override with a version that always evaluates its argument (no NDEBUG).
+#include "FreeRTOSConfig.h"
+#undef  configASSERT
+#define configASSERT(x) do { if (!(x)) { \
+    fprintf(stderr, "ASSERT %s:%d: %s\n", __FILE__, __LINE__, #x); abort(); \
+} } while(0)
 #define pdTRUE          1
 #define pdFALSE         0
 #define pdPASS          1
@@ -20,6 +28,7 @@ typedef void* TaskHandle_t;
 #define taskEXIT_CRITICAL()
 #define taskDISABLE_INTERRUPTS()
 #define taskENABLE_INTERRUPTS()
+#define portYIELD_FROM_ISR(x) do { (void)(x); } while(0)
 size_t xPortGetFreeHeapSize(void);
 size_t xPortGetMinimumEverFreeHeapSize(void);
 void* pvPortMalloc(size_t xSize);
