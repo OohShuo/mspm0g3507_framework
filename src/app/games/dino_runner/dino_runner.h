4196963765f9c0bc7cce1be7ae25cd80afdc8811
shuo <3292226889@qq.com>
2026-06-16 19:34:06 +0800

    小恐龙 and 呆呆鸟

---
+++ src/app/games/dino_runner/dino_runner.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "game_runtime.h"
+
+void Dino_Runner_Init(const Game_hardware* hardware);
+Game_result Dino_Runner_Update(const Game_input* input);
+uint32_t Dino_Runner_Get_Score(void);
+uint8_t Dino_Runner_Is_Finished(void);
