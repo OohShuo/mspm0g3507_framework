f26c99f7e58c54c8be5970e57879654cab1a0641
shuo <3292226889@qq.com>
2026-06-16 17:12:20 +0800

    界面小优化

--- src/vm/input_vm.h
+++ src/vm/input_vm.h
@@ -9,7 +9,7 @@ void Vm_Input_Init(void);
 void Vm_Input_Poll(void);
 
 // Thread-safe: read the snapshot, can be called from any thread.
-float   Vm_Input_Get_X(void);
-float   Vm_Input_Get_Y(void);
+float Vm_Input_Get_X(void);
+float Vm_Input_Get_Y(void);
 uint8_t Vm_Input_Get_Button(void);
-int     Vm_Input_Quit_Requested(void);
+int Vm_Input_Quit_Requested(void);
