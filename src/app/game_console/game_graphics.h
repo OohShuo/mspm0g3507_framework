03b1c715f729c1b9d3d9134bcea55e01657c95d7
shuo <3292226889@qq.com>
2026-06-16 23:33:58 +0800

    Merge branch 'feat-newgames'

--- src/app/game_console/game_graphics.h
+++ src/app/game_console/game_graphics.h
@@ -11,3 +11,4 @@ void Game_Graphics_Draw_Text(
     St7789* lcd, int32_t x, int32_t y, const char* text, uint8_t scale, uint16_t color);
 void Game_Graphics_Draw_U32(
     St7789* lcd, int32_t x, int32_t y, uint32_t value, uint8_t digits, uint8_t scale, uint16_t color);
+void Game_Graphics_Draw_Bitmap(St7789* lcd, int32_t x, int32_t y, int32_t w, int32_t h, const uint16_t* data);
