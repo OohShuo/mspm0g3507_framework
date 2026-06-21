366909f5e67da7f62eb8d6eef57279564ebec1d7
shuo <3292226889@qq.com>
2026-06-21 14:54:14 +0800

    feat: simulate vibration feedback in VM

---
+++ src/vm/haptics_vm.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <SDL2/SDL.h>
+
+void Vm_Haptics_Init(void);
+void Vm_Haptics_Deinit(void);
+void Vm_Haptics_Handle_Event(const SDL_Event* event);
+void Vm_Haptics_Update(void);
+void Vm_Haptics_Set_Strength(uint8_t strength_percent);
+uint8_t Vm_Haptics_Get_Strength(void);
