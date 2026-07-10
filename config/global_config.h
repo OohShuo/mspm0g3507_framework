69a0ecc085cad3772665b34366e5b318fe1d78cc
shuo <3292226889@qq.com>
2026-07-11 03:19:28 +0800

    v1.5.0

--- config/global_config.h
+++ config/global_config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define APP_VERSION_MAJOR  1u
-#define APP_VERSION_MINOR  4u
-#define APP_VERSION_PATCH  2u
-#define APP_VERSION_STRING "v1.4.2"
+#define APP_VERSION_MINOR  5u
+#define APP_VERSION_PATCH  0u
+#define APP_VERSION_STRING "v1.5.0"
